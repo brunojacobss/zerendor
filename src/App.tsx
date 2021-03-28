1e81afce1e5c862611bc8b90761902a9bc45b7a6
brunojacobss <brunojacobsschulz@gmail.com>
2021-03-28 16:52:41 -0500

    Design update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <h3>Z</h3>
+        <div className="App-body">
+          <p>WORK IN PROGRESS.</p>
+          <button>CONTACT</button>
+        </div>
+      </div>
     </div>
   );
 }
