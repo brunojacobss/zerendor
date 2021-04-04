47e44e0d16d2badccd2bd4ce9a1349e135e7da22
brunojacobss <brunojacobsschulz@gmail.com>
2021-04-04 16:01:54 -0500

    Add new font

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <div className="App-header">
-        <h3>Z</h3>
+        <h3>Zerendor</h3>
         <div className="App-body">
           <p>WORK IN PROGRESS.</p>
           <a
