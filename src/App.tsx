6e083cd399d9271c18f594ca8ccf2eac0d3fcce9
Bruno Jacobs <brunojacobsschulz@gmail.com>
2021-11-02 20:51:17 -0500

    Update design

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <div className="App-header">
-        <h3>Zerendor</h3>
-        <div className="App-body">
-          <p>WORK IN PROGRESS.</p>
-          <a
-            className="button"
-            href="https://i1.sndcdn.com/artworks-Uii8SMJvNPxy8ePA-romBoQ-t500x500.jpg"
-          >
-            CONTACT
-          </a>
-        </div>
-      </div>
-    </div>
-  );
+  return <div className="App" />;
 }
 
 export default App;
